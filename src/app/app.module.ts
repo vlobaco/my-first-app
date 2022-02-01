cf4ed088efbc98ce1b688e69550fa4f822b272a6
Victor <victor@lobaco.es>
2022-02-01 16:42:29 +0100

    Two-way binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
+
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent
+    ServerComponent,
+    ServersComponent
   ],
   imports: [
+    FormsModule, 
     BrowserModule
   ],
   providers: [],
---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  createServerStatus  = "Server no created!";
+  serverName="TestName";
+
+  constructor() { 
+    setTimeout(() => {
+      this.allowNewServer=true;
+    }, 2000);
+  }
+
+  ngOnInit(): void {
+  }
+
+  onCreateServer() {
+    this.createServerStatus="Server created!";
+  }
+
+  onServerNameUpdate(event: Event){
+    this.serverName=(<HTMLInputElement>event.target).value;
+  }
+}
